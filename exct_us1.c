03380ac4fa8b6ea66cf87f81a29260d0a31c8615
SaraYamil <43785364+SaraYamil@users.noreply.github.com>
2023-06-27 15:54:26 +0100

    Update exct_us1.c

--- exct_us1.c
+++ exct_us1.c
@@ -1,43 +1,43 @@
 #include "main.h"
 
 /**
- * _wch - Locates a command.
+ * _whch - Locates a command.
  * @command: Command name.
  * @_env: Environment variable.
  *
  * Return: Location of the command.
  */
-char *_wch(char *command, char **_env)
+char *_whch(char *command, char **_env)
 {
 	char *path_name, *ptrpath, *tkn_path, *directory;
 	int length_dir, length_cmd, index;
 	struct stat st;
 
-	path_name = getenvrn("PATH", _env);
+	path_name = get_environ("PATH", _env);
 	if (path_name)
 	{
-		ptrpath = _strdupp(path_name);
-		length_cmd = _strlength(command);
-		tkn_path = _strtiktok(ptrpath, ":");
+		ptrpath = _strdup(path_name);
+		length_cmd = _strlen(command);
+		tkn_path = _strtok(ptrpath, ":");
 		index = 0;
 		while (tkn_path != NULL)
 		{
-			if (chckcdir(path_name, &index))
+			if (check_cdir(path_name, &index))
 				if (stat(command, &st) == 0)
 					return (command);
-			length_dir = _strlength(tkn_path);
+			length_dir = _strlen(tkn_path);
 			directory = malloc(length_dir + length_cmd + 2);
-			_strcopy(directory, tkn_path);
-			_strccaat(directory, "/");
-			_strccaat(directory, command);
-			_strccaat(directory, "\0");
+			_strcpy(directory, tkn_path);
+			_strcat(directory, "/");
+			_strcat(directory, command);
+			_strcat(directory, "\0");
 			if (stat(directory, &st) == 0)
 			{
 				free(ptrpath);
 				return (directory);
 			}
 			free(directory);
-			tkn_path = _strtiktok(NULL, ":");
+			tkn_path = _strtok(NULL, ":");
 		}
 		free(ptrpath);
 		if (stat(command, &st) == 0)
@@ -51,13 +51,13 @@ char *_wch(char *command, char **_env)
 }
 
 /**
- * chckcdir - Checks if ":" is present in the current directory.
+ * check_cdir - Checks if ":" is present in the current directory.
  * @filepath: Pointer to a character string representing the filepath.
  * @index: Pointer to an integer representing the index.
  *
  * Return: 1 if the path is searchable in the current directory, 0 otherwise.
  */
-int chckcdir(char *filepath, int *index)
+int check_cdir(char *filepath, int *index)
 {
 	if (filepath[*index] == ':')
 		return (1);
@@ -74,26 +74,26 @@ int chckcdir(char *filepath, int *index)
 }
 
 /**
- * chck_errcommnd - Verifies if the user has permissions
+ * check_error_command - Verifies if the user has permissions
  *                      to access a command or directory.
  * @dir_dest: Destination directory.
  * @datashell: Data structure.
  *
  * Return: 1 if there is an error, 0 if not.
  */
-int chck_errcommnd(char *dir_dest, shll_comm *datashell)
+int check_error_command(char *dir_dest, shll_comm *datashell)
 {
 	if (dir_dest == NULL)
 	{
-		geterr(datashell, 127);
+		get_err(datashell, 127);
 		return (1);
 	}
 
-	if (_strccmmpp(datashell->args[0], dir_dest) != 0)
+	if (_strcmp(datashell->args[0], dir_dest) != 0)
 	{
 		if (access(dir_dest, X_OK) == -1)
 		{
-			geterr(datashell, 126);
+			get_err(datashell, 126);
 			free(dir_dest);
 			return (1);
 		}
@@ -103,7 +103,7 @@ int chck_errcommnd(char *dir_dest, shll_comm *datashell)
 	{
 		if (access(datashell->args[0], X_OK) == -1)
 		{
-			geterr(datashell, 126);
+			get_err(datashell, 126);
 			return (1);
 		}
 	}
@@ -112,25 +112,25 @@ int chck_errcommnd(char *dir_dest, shll_comm *datashell)
 }
 
 /**
- * cmmnd_ex - Executes command lines.
+ * command_exec - Executes command lines.
  * @datashell: Data relevant (args and input).
  *
  * Return: 1 on success.
  */
-int cmmnd_ex(shll_comm *datashell)
+int command_exec(shll_comm *datashell)
 {
 	pid_t pidm, wpid;
 	int stte, exc;
 	char *dir;
 	(void)wpid;
 
-	exc = isexc(datashell);
+	exc = is_exec(datashell);
 	if (exc == -1)
 		return (1);
 	if (exc == 0)
 	{
-		dir = _wch(datashell->args[0], datashell->_env);
-		if (chck_errcommnd(dir, datashell) == 1)
+		dir = _whch(datashell->args[0], datashell->_env);
+		if (check_error_command(dir, datashell) == 1)
 			return (1);
 	}
 
@@ -138,7 +138,7 @@ int cmmnd_ex(shll_comm *datashell)
 	if (pidm == 0)
 	{
 		if (exc == 0)
-			dir = _wch(datashell->args[0], datashell->_env);
+			dir = _whch(datashell->args[0], datashell->_env);
 		else
 			dir = datashell->args[0];
 		execve(dir + exc, datashell->args, datashell->_env);
@@ -161,12 +161,12 @@ int cmmnd_ex(shll_comm *datashell)
 }
 
 /**
- * isexc - Determines if a command is an executable.
+ * is_exec - Determines if a command is an executable.
  * @datashell: Data structure.
  *
  * Return: 0 if it's not an executable, otherwise a positive number.
  */
-int isexc(shll_comm *datashell)
+int is_exec(shll_comm *datashell)
 {
 	struct stat status;
 	int index;
@@ -199,7 +199,7 @@ int isexc(shll_comm *datashell)
 
 	if (stat(inp + index, &status) == 0)
 		return (index);
-	geterr(datashell, 127);
+	get_err(datashell, 127);
 
 	return (-1);
 }
