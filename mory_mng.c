69e15821168da86631f936a220daac1170a5f0ff
SaraYamil <43785364+SaraYamil@users.noreply.github.com>
2023-06-27 16:00:31 +0100

    Update mory_mng.c

--- mory_mng.c
+++ mory_mng.c
@@ -1,114 +1,143 @@
 #include "main.h"
 
 /**
- * add_lnndend - Adds a command line at the end of a line_list.
- * @hd: Head of the linked list.
- * @cmd_line: Command line.
+ * get_err - Calls the error message according to the
+ *	   builtin, syntax, or permission
+ * @datashell: Data structure that contains arguments
+ * @error_val: Error value
  *
- * Return: Address of the head.
+ * Return: Error value
  */
-line_list *add_lnndend(line_list **hd, char *cmd_line)
+int get_err(shll_comm *datashell, int error_val)
 {
-	line_list *nw, *tmp;
+	char *error;
 
-	nw = malloc(sizeof(line_list));
-	if (nw == NULL)
-		return (NULL);
-
-	nw->line = cmd_line;
-	nw->next = NULL;
-	tmp = *hd;
-
-	if (tmp == NULL)
+	switch (error_val)
 	{
-		*hd = nw;
+	case -1:
+		error = err_environ(datashell);
+		break;
+	case 126:
+		error = err_path126(datashell);
+		break;
+	case 127:
+		error = error_404(datashell);
+		break;
+	case 2:
+		if (_strcmp("exit", datashell->args[0]) == 0)
+			error = err_shell_exit(datashell);
+		else if (_strcmp("cd", datashell->args[0]) == 0)
+			error = err_gcd(datashell);
+		break;
 	}
+	if (error)
+	{
+		write(STDERR_FILENO, error, _strlen(error));
+		free(error);
+	}
+	datashell->stat = error_val;
+	return (error_val);
+}
+
+/**
+ * empty_data - frees data structure.
+ * @data_shell: data structure.
+ *
+ * Return: no return.
+ */
+void empty_data(shll_comm *data_shell)
+{
+	unsigned int index;
+
+	for (index = 0; data_shell->_env[index]; index++)
+		free(data_shell->_env[index]);
+
+	free(data_shell->_env);
+	free(data_shell->pid);
+}
+
+/**
+ * set_datashell - Initialize data structure
+ * @data_shell: data structure
+ * @argv: argument vector
+ *
+ * Return: no return
+ */
+void set_datashell(shll_comm *data_shell, char **argv)
+{
+	unsigned int i;
+
+	data_shell->argv = argv;
+	data_shell->input = NULL;
+	data_shell->args = NULL;
+	data_shell->stat = 0;
+	data_shell->counter = 1;
+	for (i = 0; environ[i]; i++)
+		;
+	data_shell->_env = malloc(sizeof(char *) * (i + 1));
+	for (i = 0; environ[i]; i++)
+	{
+		data_shell->_env[i] = _strdup(environ[i]);
+	}
+	data_shell->_env[i] = NULL;
+	data_shell->pid = conv_itoa(getpid());
+}
+
+/**
+ * get_hlp - Function that retrieves help messages
+ *	   according to built-in command
+ * @data_shll: Data structure (args and input)
+ *
+ * Return: 1
+ */
+int get_hlp(shll_comm *data_shll)
+{
+
+	if (data_shll->args[1] == 0)
+		disp_help_general();
+	else if (_strcmp(data_shll->args[1], "setenv") == 0)
+		disp_help_setenv();
+	else if (_strcmp(data_shll->args[1], "env") == 0)
+		disp_help_env();
+	else if (_strcmp(data_shll->args[1], "unsetenv") == 0)
+		display_unsetenv();
+	else if (_strcmp(data_shll->args[1], "help") == 0)
+		disp_help();
+	else if (_strcmp(data_shll->args[1], "exit") == 0)
+		disp_help_exit();
+	else if (_strcmp(data_shll->args[1], "cd") == 0)
+		disp_help_cd();
+	else if (_strcmp(data_shll->args[1], "alias") == 0)
+		disp_help_alias();
 	else
-	{
-		while (tmp->next != NULL)
-			tmp = tmp->next;
-		tmp->next = nw;
-	}
-
-	return (*hd);
+		write(STDERR_FILENO, data_shll->args[0],
+		      _strlen(data_shll->args[0]));
+	data_shll->stat = 0;
+	return (1);
 }
 
 /**
- * free_lnls - Frees a line_list.
- * @hd: Head of the linked list.
+ * get_bltn - Retrieves the function pointer of the builtin command
+ * @command: Command line
  *
- * Return: No return.
+ * Return: Function pointer of the builtin command
  */
-void free_lnls(line_list **hd)
+int (*get_bltn(char *command))(shll_comm *)
 {
-	line_list *tmp;
-	line_list *current;
+	builtin_t bltn[] = {
+	    {"env", display_env},
+	    {"exit", exit_sh},
+	    {"setenv", _setenv},
+	    {"unsetenv", _unsetenv},
+	    {"cd", changedir_shell},
+	    {"help", get_hlp},
+	    {NULL, NULL}};
+	int index;
 
-	if (hd != NULL)
+	for (index = 0; bltn[index].commname; index++)
 	{
-		current = *hd;
-		while ((tmp = current) != NULL)
-		{
-			current = current->next;
-			free(tmp);
-		}
-		*hd = NULL;
+		if (_strcmp(bltn[index].commname, command) == 0)
+			break;
 	}
-}
-
-/**
- * add_noden - Adds a separator found at the end
- * of a sep_list.
- * @hd: Head of the linked list.
- * @sp: Separator found (; | &).
- *
- * Return: Address of the head.
- */
-sep_list *add_noden(sep_list **hd, char sp)
-{
-	sep_list *new_n, *tmp;
-
-	new_n = malloc(sizeof(sep_list));
-	if (new_n == NULL)
-		return (NULL);
-
-	new_n->sep = sp;
-	new_n->next = NULL;
-	tmp = *hd;
-
-	if (tmp == NULL)
-	{
-		*hd = new_n;
-	}
-	else
-	{
-		while (tmp->next != NULL)
-			tmp = tmp->next;
-		tmp->next = new_n;
-	}
-
-	return (*hd);
-}
-
-/**
- * free_spls - Frees a sep_list.
- * @hd: Head of the linked list.
- *
- * Return: No return.
- */
-void free_spls(sep_list **hd)
-{
-	sep_list *tmp;
-	sep_list *current;
-
-	if (hd != NULL)
-	{
-		current = *hd;
-		while ((tmp = current) != NULL)
-		{
-			current = current->next;
-			free(tmp);
-		}
-		*hd = NULL;
-	}
+	return (bltn[index].f);
 }
