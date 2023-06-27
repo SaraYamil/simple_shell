1907113c5f9d6ceea744800d84069df42e4f9546
SaraYamil <43785364+SaraYamil@users.noreply.github.com>
2023-06-27 15:52:32 +0100

    Update err_hand1.c

--- err_hand1.c
+++ err_hand1.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * frstchrctr - finds the index of the first non-whitespace character.
+ * first_character - finds the index of the first non-whitespace character.
  * @inp: intptr_t string.
  * @index: pointer to the index variable.
  *
  * Return: 1 if there is an error, 0 otherwise.
  */
-int frstchrctr(char *inp, int *index)
+int first_character(char *inp, int *index)
 {
 
 	for (*index = 0; inp[*index]; *index += 1)
@@ -25,7 +25,7 @@ int frstchrctr(char *inp, int *index)
 }
 
 /**
- * dspsynerr - displays a syntax error message.
+ * disp_syn_err - displays a syntax error message.
  * @data_sh: data structure.
  * @inp: intptr_t string.
  * @index: index of the error.
@@ -33,7 +33,7 @@ int frstchrctr(char *inp, int *index)
  *
  * Return: No return value.
  */
-void dspsynerr(shll_comm *data_sh, char *inp, int index, int boolean)
+void disp_syn_err(shll_comm *data_sh, char *inp, int index, int boolean)
 {
 	char *msg, *msg2, *msg3, *error, *counter;
 	int length;
@@ -54,9 +54,9 @@ void dspsynerr(shll_comm *data_sh, char *inp, int index, int boolean)
 
 	msg2 = ": Syntax error: \"";
 	msg3 = "\" unexpected\n";
-	counter = convitoa(data_sh->counter);
-	length = _strlength(data_sh->argv[0]) + _strlength(counter);
-	length += _strlength(msg) + _strlength(msg2) + _strlength(msg3) + 2;
+	counter = conv_itoa(data_sh->counter);
+	length = _strlen(data_sh->argv[0]) + _strlen(counter);
+	length += _strlen(msg) + _strlen(msg2) + _strlen(msg3) + 2;
 
 	error = malloc(sizeof(char) * (length + 1));
 	if (error == 0)
@@ -64,13 +64,13 @@ void dspsynerr(shll_comm *data_sh, char *inp, int index, int boolean)
 		free(counter);
 		return;
 	}
-	_strcopy(error, data_sh->argv[0]);
-	_strccaat(error, ": ");
-	_strccaat(error, counter);
-	_strccaat(error, msg2);
-	_strccaat(error, msg);
-	_strccaat(error, msg3);
-	_strccaat(error, "\0");
+	_strcpy(error, data_sh->argv[0]);
+	_strcat(error, ": ");
+	_strcat(error, counter);
+	_strcat(error, msg2);
+	_strcat(error, msg);
+	_strcat(error, msg3);
+	_strcat(error, "\0");
 
 	write(STDERR_FILENO, error, length);
 	free(error);
@@ -78,27 +78,27 @@ void dspsynerr(shll_comm *data_sh, char *inp, int index, int boolean)
 }
 
 /**
- * chsynerr - intermediate function to find and print a syntax error.
+ * ch_syn_err - intermediate function to find and print a syntax error.
  * @data_sh: data structure.
  * @inptrt: intptr_t string.
  *
  * Return: 1 if there is an error, 0 otherwise.
  */
-int chsynerr(shll_comm *data_sh, char *inptrt)
+int ch_syn_err(shll_comm *data_sh, char *inptrt)
 {
 	int start = 0, fcharac = 0, index = 0;
 
-	fcharac = frstchrctr(inptrt, &start);
+	fcharac = first_character(inptrt, &start);
 	if (fcharac == -1)
 	{
-		dspsynerr(data_sh, inptrt, start, 0);
+		disp_syn_err(data_sh, inptrt, start, 0);
 		return (1);
 	}
 
-	index = sepopererr(inptrt + start, 0, *(inptrt + start));
+	index = sep_oper_error(inptrt + start, 0, *(inptrt + start));
 	if (index != 0)
 	{
-		dspsynerr(data_sh, inptrt, start + index, 1);
+		disp_syn_err(data_sh, inptrt, start + index, 1);
 		return (1);
 	}
 
@@ -106,29 +106,29 @@ int chsynerr(shll_comm *data_sh, char *inptrt)
 }
 
 /**
- * duplchrs - counts the repetitions of a character in a string.
+ * dupl_chars - counts the repetitions of a character in a string.
  * @inp: intptr_t string.
  * @index: index.
  *
  * Return: The number of repetitions.
  */
-int duplchrs(char *inp, int index)
+int dupl_chars(char *inp, int index)
 {
 	if (*(inp - 1) == *inp)
-		return (duplchrs(inp - 1, index + 1));
+		return (dupl_chars(inp - 1, index + 1));
 
 	return (index);
 }
 
 /**
- * sepopererr - finds syntax errors in the intptr_t string.
+ * sep_oper_error - finds syntax errors in the intptr_t string.
  * @inp: intptr_t string.
  * @index: index.
  * @lastchar: last character read.
  *
  * Return: The index of the error. Returns 0 when there are no errors.
  */
-int sepopererr(char *inp, int index, char lastchar)
+int sep_oper_error(char *inp, int index, char lastchar)
 {
 	int counter = 0;
 
@@ -136,7 +136,7 @@ int sepopererr(char *inp, int index, char lastchar)
 		return (0);
 
 	if (*inp == ' ' || *inp == '\t')
-		return (sepopererr(inp + 1, index + 1, lastchar));
+		return (sep_oper_error(inp + 1, index + 1, lastchar));
 
 	if (*inp == ';')
 		if (lastchar == '|' || lastchar == '&' || lastchar == ';')
@@ -149,7 +149,7 @@ int sepopererr(char *inp, int index, char lastchar)
 
 		if (lastchar == '|')
 		{
-			counter = duplchrs(inp, 0);
+			counter = dupl_chars(inp, 0);
 			if (counter == 0 || counter > 1)
 				return (index);
 		}
@@ -162,11 +162,11 @@ int sepopererr(char *inp, int index, char lastchar)
 
 		if (lastchar == '&')
 		{
-			counter = duplchrs(inp, 0);
+			counter = dupl_chars(inp, 0);
 			if (counter == 0 || counter > 1)
 				return (index);
 		}
 	}
 
-	return (sepopererr(inp + 1, index + 1, *inp));
+	return (sep_oper_error(inp + 1, index + 1, *inp));
 }
