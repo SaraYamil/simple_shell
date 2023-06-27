38a631d669c0783d4e61dcddec9911b79a7850c8
sara yamil <sarayamil18@gmail.com>
2023-06-27 16:52:33 -0700

    sara

--- main.h
+++ main.h
@@ -16,16 +16,16 @@
 #define TOK_BUFSIZE 128
 #define TOK_DELIM " \t\r\n\a"
 
-/* environ macro */
+/* VVVVVVVVVVVVo */
 extern char **environ;
 
 /**
- * struct sep_list_s - Single linked list node to store separators
- * @sep: The separator (;, |, or &)
- * @next: Pointer to the next node in the linked list
- * Description: This data structure represents a single
- *              linked list node
- *              used to store separators in the shell.
+ * struct sep_list_s - BBBB BBBBBBB BBBBBBB BBBBB BBBBB BBBBBBB BB
+ * @sep: HHHHHHHH HHHH (;, |, or &)
+ * @next: MMMM hhhhhhh hhhhhh hhhhh hhhhh hhhhh hhhhh
+ * Description: hhhhhh hhhhh hhhhhh hhhhhhhhh hhhhh hhh
+ *              hhhhhhhhhh hhhhh
+ *              zzzzzzzzzzz z hhhhhhh hhhhhhhh hhhhhl.
  */
 typedef struct sep_list_s
 {
@@ -34,14 +34,14 @@ typedef struct sep_list_s
 } sep_list;
 
 /**
- * struct line_ls_s - Single linked list node to store
- *                    command lines
- * @line: The command line
- * @next: Pointer to the next node in the linked list
+ * struct line_ls_s - mmmmm mmmmmmm mmmmmm mmmmm mmmmm
+ *                    zzzzzzz zzzzz
+ * @line: ddddddd dddd ddd
+ * @next: pppppppp pppppppppp oooo ooo ooo ooooo oooo
  *
- * Description: This data structure represents a single
- *              linked list node
- *              used to store command lines in the shell.
+ * Description: ddddd bbbbbbbb bbbbbbbbb bbbbbb bbbbbbe
+ *              mmmmmm lllll lll
+ *              uuuuuuu uuuuuuuuuu uuuu uuuuu uuuuuu uuuu
  */
 typedef struct line_ls_s
 {
@@ -50,15 +50,15 @@ typedef struct line_ls_s
 } line_list;
 
 /**
- * struct shell_dt - Contains relevant data
- *                     for the shell during runtime
- * @argv: The argument vector (command-line arguments)
- * @input: The command line input provided by the user
- * @args: The tokens extracted from the command line
- * @stat: The last status of the shell
- * @counter: The line counter for tracking the number of lines processed
- * @_env: The environment variables
- * @pid: The process ID of the shell
+ * struct shell_dt - Czzz zzzzzzzzzz zzzzzz
+ *                     ffffffff ff fffffff ffffff f
+ * @argv: ffffffffffff fffffffffff fffffffff ffffgggss
+ * @input: Tss ssssssssssss sssssssss sssssss ssssssss
+ * @args: dddddddddd ddddddddddd dddddddddd dddddd d
+ * @stat: xxxxxxxxxx xxxxxxxx x xxxxxl
+ * @counter: kkkkkkkkkk kkkkkkkkkk kkkkkkkkk kkkk kkkkkkkk kkkkkkkkkk kk
+ * @_env: hhhhh hhhhhhhhhh hhhhhhhs
+ * @pid: ssssssss sssss ssssss sssss
  */
 typedef struct shell_dt
 {
@@ -72,15 +72,15 @@ typedef struct shell_dt
 } shll_comm;
 
 /**
- * struct rvar_ls - Single linked list node to store variables
- * @len_var: Length of the variable
- * @val: Value of the variable
- * @len_val: Length of the value
- * @next: Pointer to the next node in the linked list
+ * struct rvar_ls - vvvvvvv vvvvvvvvvv vvvvvvvvvvvvvv vvvvvvvv
+ * @len_var: dddddddd dddddddd dddd
+ * @val: ddddddd ddddddddd ddd
+ * @len_val: gggggg ggg ggggggue
+ * @next: zzzzzzzzzzz zzzzzzzzzzzz zz zzzzzz zzzz zzz
  *
- * Description: This data structure represents a single linked
- *              list node
- *              used to store variables in the shell.
+ * Description: zzzzzzzz zzzzzzzzzzzzz zzzzzzzzzzz zzzzzzz zzz
+ *              xxxt xxxe
+ *              mmmmm mmm mmmm mmmm mmm mmm mmm mmm m
  */
 typedef struct rvar_ls
 {
@@ -91,13 +91,12 @@ typedef struct rvar_ls
 } r_var;
 
 /**
- * struct builtin_s - Builtin command structure
- * @commname: The name of the command builtin (e.g., cd, exit, env)
- * @f: Pointer to the function associated with the command
+ * struct builtin_s - ssssssssss ssssss sss sss
+ * @commname: dddddddddd ddddddddddd ddddddddddddd dddddddddd ddd d
+ * @f: ppppppp pppppppppp pp pp  nnn nnnn nnn nn nnn nnn nn
  *
- * Description: This structure represents a builtin command in the shell.
- * It contains the name of the command and a pointer to the function that
- * implements the command functionality.
+ * Description: ddddddd dkkkk kkk kkkk kkkkkk kkkkk kkkkk kkkk kkk kkk kk 
+ * dddddddd dddddddddd ddddddd ddddddddddddd ddddddddddd dddd dddddd that
  */
 typedef struct builtin_s
 {
@@ -105,7 +104,7 @@ typedef struct builtin_s
 	int (*f)(shll_comm *datash);
 } builtin_t;
 
-/* Linked lists functions */
+/* gd gd gd gd gd g dgd gd*/
 sep_list *add_node_en(sep_list **head, char sep);
 void free_sp_ls(sep_list **head);
 line_list *add_ln_nd_end(line_list **head, char *line);
@@ -113,7 +112,7 @@ void free_line_ls(line_list **head);
 r_var *add_var_nd(r_var **head, int lvar, char *var, int lval);
 void free_value_ls(r_var **head);
 
-/* String functions protytypes */
+/* string hhh h h h h h h h h h*/
 char *_strcat(char *dest, const char *src);
 char *_strcpy(char *dest, char *src);
 void _memcpy(void *newptr, const void *ptr, unsigned int size);
@@ -132,7 +131,7 @@ char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size);
 int _strcmp(char *s1, char *s2);
 char *_strchr(char *s, char c);
 
-/* Error handling functions prototypes */
+/* hbjhbjvusvv js ghvs jsvvsv shvubsjjs*/
 int dupl_chars(char *input, int i);
 int sep_oper_error(char *input, int i, char last);
 int first_character(char *input, int *i);
@@ -141,10 +140,10 @@ int ch_syn_err(shll_comm *data_sh, char *input);
 char *without_cmt(char *in);
 void looping_shll(shll_comm *datash);
 
-/* read_line function protytype */
+/* nbshbjbsbubsjhkhsb s bhs hse */
 char *read_line(int *i_eof);
 
-/* Environmant variables function protytypes */
+/* zhbzbhz jz  z z z uhk zz jz jz j zj jz jz */
 int verify_vars(r_var **h, char *in, char *st, shll_comm *data);
 char *rpl_inp(r_var **head, char *input, char *new_input, int nlen);
 char *get_environ(const char *name, char **_env);
@@ -157,18 +156,18 @@ int compare_envname(const char *name_env, const char *name_ptr);
 void verify_env(r_var **h, char *in, shll_comm *data);
 char *replace_str(char *input, shll_comm *datash);
 
-/* Split functions protytypes */
+/* pappapapap apapap papappa  */
 char *swp_character(char *input, int bool);
 void move_nxt(sep_list **list_s, line_list **list_l, shll_comm *datash);
 int split_cmds(shll_comm *datash, char *input);
 void add_nd(sep_list **head_s, line_list **head_l, char *input);
 char **splt_ln(char *input);
 
-/* get_line functions protytypes */
+/* mamama mamama almamama mamama */
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 void get_ln(char **lineptr, size_t *n, char *buffer, size_t j);
 
-/* Execute functions protytypes */
+/* abjhaj abak ,akba kabjknjkks */
 int execute_line(shll_comm *datash);
 int check_error_command(char *dir, shll_comm *datash);
 int command_exec(shll_comm *datash);
@@ -176,22 +175,22 @@ int check_cdir(char *path, int *i);
 char *_whch(char *cmd, char **_env);
 int is_exec(shll_comm *datash);
 
-/* cd functions protypes */
+/* pappapa pappapa ppaps */
 void chandir_dor(shll_comm *datash);
 void cd_to_dist(shll_comm *datash);
 void changedir_prev(shll_comm *datash);
 void changedir_to_home(shll_comm *datash);
 int changedir_shell(shll_comm *datash);
 
-/* main functions protytypes*/
+/* mamama mamama mamamma mma*/
 void empty_data(shll_comm *datash);
 void set_datashell(shll_comm *datash, char **av);
 
-/* built-in functions prototypes */
+/* hahbah ha ahbahb bajajajajj j */
 int (*get_bltn(char *cmd))(shll_comm *datash);
 int exit_sh(shll_comm *datash);
 
-/* Error handling functions prototypes */
+/* Eddhbd hdh hd hd hhhh hhhhd hhhhdpp */
 int get_err(shll_comm *datash, int eval);
 void get_sigint(int sig);
 char *error_get_alias(char **args);
@@ -204,7 +203,7 @@ char *err_gcd(shll_comm *datash);
 char *error_404(shll_comm *datash);
 char *err_shell_exit(shll_comm *datash);
 
-/* Help functions prototypes */
+/* zzzz z  zzzzzz zzzzzzypes */
 void disp_help_env(void);
 void disp_help_setenv(void);
 void display_unsetenv(void);
