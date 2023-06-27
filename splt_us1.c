44ff94f4f896d131ba42aaa62a7d0fc2f9414ca9
SaraYamil <43785364+SaraYamil@users.noreply.github.com>
2023-06-27 16:01:43 +0100

    Update splt_us1.c

--- splt_us1.c
+++ splt_us1.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * mv_next - Moves to the next command line stored.
+ * move_nxt - Moves to the next command line stored.
  * @lis_s: Separator list.
  * @lis_l: Command line list.
  * @data_shell: Data structure.
  *
  * Return: No return value.
  */
-void mv_next(sep_list **lis_s, line_list **lis_l, shll_comm *data_shell)
+void move_nxt(sep_list **lis_s, line_list **lis_l, shll_comm *data_shell)
 {
 	int loop_sep;
 	sep_list *ls_s;
@@ -42,7 +42,7 @@ void mv_next(sep_list **lis_s, line_list **lis_l, shll_comm *data_shell)
 }
 
 /**
- * swp_chrct - Swaps '|' and '&' for non-printed characters
+ * swp_character - Swaps '|' and '&' for non-printed characters
  *                 in a string.
  * @inp: Input string.
  * @boolean: Type of swap. If boolean is 0, swaps '|' and '&'
@@ -50,7 +50,7 @@ void mv_next(sep_list **lis_s, line_list **lis_l, shll_comm *data_shell)
  *
  * Return: Swapped string.
  */
-char *swp_chrct(char *inp, int boolean)
+char *swp_character(char *inp, int boolean)
 {
 	int i;
 
@@ -87,62 +87,62 @@ char *swp_chrct(char *inp, int boolean)
 }
 
 /**
- * addnd - Adds separators and command lines to the lists.
+ * add_nd - Adds separators and command lines to the lists.
  * @hd_s: Head of the separator list.
  * @hd_l: Head of the command lines list.
  * @inp: Input string.
  *
  * Return: No return value.
  */
-void addnd(sep_list **hd_s, line_list **hd_l, char *inp)
+void add_nd(sep_list **hd_s, line_list **hd_l, char *inp)
 {
 	int i;
 	char *command_line;
 
-	inp = swp_chrct(inp, 0);
+	inp = swp_character(inp, 0);
 
 	for (i = 0; inp[i]; i++)
 	{
 		if (inp[i] == ';')
-			addnd(hd_s, inp[i]);
+			add_node_en(hd_s, inp[i]);
 
 		if (inp[i] == '|' || inp[i] == '&')
 		{
-			addnd(hd_s, inp[i]);
+			add_node_en(hd_s, inp[i]);
 			i++;
 		}
 	}
 
-	command_line = _strtiktok(inp, ";|&");
+	command_line = _strtok(inp, ";|&");
 	do {
-		command_line = swp_chrct(command_line, 1);
-		add_lnndend(hd_l, command_line);
-		command_line = _strtiktok(NULL, ";|&");
+		command_line = swp_character(command_line, 1);
+		add_ln_nd_end(hd_l, command_line);
+		command_line = _strtok(NULL, ";|&");
 	} while (command_line != NULL);
 }
 
 /**
- * read_ln - Reads the input string.
+ * read_line - Reads the input string.
  * @int_eof: Return value of the getline function.
  *
  * Return: Input string.
  */
-char *read_ln(int *int_eof)
+char *read_line(int *int_eof)
 {
 	char *inp = NULL;
 	size_t buffersize = 0;
 
-	*int_eof = getln(&inp, &buffersize, stdin);
+	*int_eof = getline(&inp, &buffersize, stdin);
 
 	return (inp);
 }
 
 /**
- * free_valls - Frees a r_var list.
+ * free_value_ls - Frees a r_var list.
  * @hd: Head of the linked list.
  * Return: No return.
  */
-void free_valls(r_var **hd)
+void free_value_ls(r_var **hd)
 {
 	r_var *tmp;
 	r_var *current;
