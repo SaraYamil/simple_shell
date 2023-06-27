30c2892fc25ca78be9aaa2682a8002c37792cd8d
SaraYamil <43785364+SaraYamil@users.noreply.github.com>
2023-06-27 13:33:36 +0100

    sara

--- hlp-us2.c
+++ hlp-us2.c
@@ -9,19 +9,19 @@ void dsp_helpgnrl(void)
 {
 	char *hlp = "^-^ bash, version 1.0(1)-release\n";
 
-	write(STDOUT_FILENO, hlp, _strlen(hlp));
+	write(STDOUT_FILENO, hlp, _strlength(hlp));
 	hlp = "These commands are defined internally.Type 'help' to see the list";
-	write(STDOUT_FILENO, hlp, _strlen(hlp));
+	write(STDOUT_FILENO, hlp, _strlength(hlp));
 	hlp = "Type 'help name' to find out more about the function 'name'.\n\n ";
-	write(STDOUT_FILENO, hlp, _strlen(hlp));
+	write(STDOUT_FILENO, hlp, _strlength(hlp));
 	hlp = " alias: alias [name=['string']]\n cd: cd [-L|[-P [-e]] [-@]] ";
-	write(STDOUT_FILENO, hlp, _strlen(hlp));
+	write(STDOUT_FILENO, hlp, _strlength(hlp));
 	hlp = "[dir]\nexit: exit [n]\n  env: env [option] [name=value] [command ";
-	write(STDOUT_FILENO, hlp, _strlen(hlp));
+	write(STDOUT_FILENO, hlp, _strlength(hlp));
 	hlp = "[args]]\n  setenv: setenv [variable] [value]\n  unsetenv: ";
-	write(STDOUT_FILENO, hlp, _strlen(hlp));
+	write(STDOUT_FILENO, hlp, _strlength(hlp));
 	hlp = "unsetenv [variable]\n";
-	write(STDOUT_FILENO, hlp, _strlen(hlp));
+	write(STDOUT_FILENO, hlp, _strlength(hlp));
 }
 /**
  * dsp_helpex - Help information fot the builint exit
@@ -32,11 +32,11 @@ void dsp_helpex(void)
 {
 	char *hlp = "exit: exit [n]\n Exit shell.\n";
 
-	write(STDOUT_FILENO, hlp, _strlen(hlp));
+	write(STDOUT_FILENO, hlp, _strlength(hlp));
 	hlp = "Exits the shell with a status of N. If N is ommited, the exit";
-	write(STDOUT_FILENO, hlp, _strlen(hlp));
+	write(STDOUT_FILENO, hlp, _strlength(hlp));
 	hlp = "statusis that of the last command executed\n";
-	write(STDOUT_FILENO, hlp, _strlen(hlp));
+	write(STDOUT_FILENO, hlp, _strlength(hlp));
 }
 
 /**
@@ -48,9 +48,9 @@ void dsp_unstnv(void)
 {
 	char *hlp = "unsetenv: unsetenv (const char *name)\n\t";
 
-	write(STDOUT_FILENO, hlp, _strlen(hlp));
+	write(STDOUT_FILENO, hlp, _strlength(hlp));
 	hlp = "Remove an entry completely from the environment\n";
-	write(STDOUT_FILENO, hlp, _strlen(hlp));
+	write(STDOUT_FILENO, hlp, _strlength(hlp));
 }
 
 /**
@@ -94,7 +94,7 @@ void getln(char **lnptr, size_t *num, char *buf, size_t size_b)
 	}
 	else
 	{
-		_strcpy(*lnptr, buf);
+		_strcopy(*lnptr, buf);
 		free(buf);
 	}
 }
