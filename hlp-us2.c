653555e85bd562548a147bad5d12c5e180f8aaa2
SaraYamil <43785364+SaraYamil@users.noreply.github.com>
2023-06-27 15:58:05 +0100

    Update hlp-us2.c

--- hlp-us2.c
+++ hlp-us2.c
@@ -1,70 +1,70 @@
 #include "main.h"
 
 /**
- * dsp_helpgnrl - Entry point for help information for the help builtin
+ * disp_help_general - Entry point for help information for the help builtin
  *
  * Return: Void
  */
-void dsp_helpgnrl(void)
+void disp_help_general(void)
 {
 	char *hlp = "^-^ bash, version 1.0(1)-release\n";
 
-	write(STDOUT_FILENO, hlp, _strlength(hlp));
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
 	hlp = "These commands are defined internally.Type 'help' to see the list";
-	write(STDOUT_FILENO, hlp, _strlength(hlp));
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
 	hlp = "Type 'help name' to find out more about the function 'name'.\n\n ";
-	write(STDOUT_FILENO, hlp, _strlength(hlp));
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
 	hlp = " alias: alias [name=['string']]\n cd: cd [-L|[-P [-e]] [-@]] ";
-	write(STDOUT_FILENO, hlp, _strlength(hlp));
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
 	hlp = "[dir]\nexit: exit [n]\n  env: env [option] [name=value] [command ";
-	write(STDOUT_FILENO, hlp, _strlength(hlp));
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
 	hlp = "[args]]\n  setenv: setenv [variable] [value]\n  unsetenv: ";
-	write(STDOUT_FILENO, hlp, _strlength(hlp));
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
 	hlp = "unsetenv [variable]\n";
-	write(STDOUT_FILENO, hlp, _strlength(hlp));
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
 }
 /**
- * dsp_helpex - Help information fot the builint exit
+ * disp_help_exit - Help information fot the builint exit
  *
  * Return: Void
  */
-void dsp_helpex(void)
+void disp_help_exit(void)
 {
 	char *hlp = "exit: exit [n]\n Exit shell.\n";
 
-	write(STDOUT_FILENO, hlp, _strlength(hlp));
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
 	hlp = "Exits the shell with a status of N. If N is ommited, the exit";
-	write(STDOUT_FILENO, hlp, _strlength(hlp));
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
 	hlp = "statusis that of the last command executed\n";
-	write(STDOUT_FILENO, hlp, _strlength(hlp));
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
 }
 
 /**
- * dsp_unstnv - Help information for the builtin unsetenv
+ * display_unsetenv - Help information for the builtin unsetenv
  *
  * Return: Void
  */
-void dsp_unstnv(void)
+void display_unsetenv(void)
 {
 	char *hlp = "unsetenv: unsetenv (const char *name)\n\t";
 
-	write(STDOUT_FILENO, hlp, _strlength(hlp));
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
 	hlp = "Remove an entry completely from the environment\n";
-	write(STDOUT_FILENO, hlp, _strlength(hlp));
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
 }
 
 /**
- * get_sgnt - Handle the crtl + c call in prompt
+ * get_sigint - Handle the crtl + c call in prompt
  * @sig: Signal handler
  */
-void get_sgnt(int sig)
+void get_sigint(int sig)
 {
 	(void)sig;
 	write(STDOUT_FILENO, "\n^-^ ", 5);
 }
 
 /**
- * getln - assigns the line var for get_line
+ * get_ln - assigns the line var for get_line
  * @lnptr: Pointer to the line variable
  * @num: Pointer to the size of line variable
  * @buf: String that is being assigned to line
@@ -72,7 +72,7 @@ void get_sgnt(int sig)
  *
  * Return: void
  */
-void getln(char **lnptr, size_t *num, char *buf, size_t size_b)
+void get_ln(char **lnptr, size_t *num, char *buf, size_t size_b)
 {
 
 	if (*lnptr == NULL)
@@ -94,7 +94,7 @@ void getln(char **lnptr, size_t *num, char *buf, size_t size_b)
 	}
 	else
 	{
-		_strcopy(*lnptr, buf);
+		_strcpy(*lnptr, buf);
 		free(buf);
 	}
 }
